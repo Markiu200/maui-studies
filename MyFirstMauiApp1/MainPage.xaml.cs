24c3e7262d9ddca513bf9b49d7aec2314f03e180
Markiu200 <markiu2007@interia.pl>
2024-07-18 00:23:30 +0200

    Add comment about what InitializeComponent() do

--- MyFirstMauiApp1/MainPage.xaml.cs
+++ MyFirstMauiApp1/MainPage.xaml.cs
@@ -6,6 +6,11 @@
 
         public MainPage()
         {
+            // InitializeComponent();
+            // "(...) which initializes all the objects defined in the XAML file, connects them all together in parent-child relationships,
+            // attaches event handlers defined in code to events set in the XAML file, and sets the resultant tree of objects as the content of the page."
+            // https://learn.microsoft.com/en-us/dotnet/maui/xaml/fundamentals/get-started?view=net-maui-8.0
+
             InitializeComponent();
         }
 
