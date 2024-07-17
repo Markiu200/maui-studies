6ca8f8fb743ecbc347eaa36a52820924d00d20ea
Markiu200 <markiu2007@interia.pl>
2024-07-17 21:24:16 +0200

    Change button example to show sender details. Just to see what is being sent as "sender".

--- MyFirstMauiApp1/TestPage.cs
+++ MyFirstMauiApp1/TestPage.cs
@@ -6,10 +6,12 @@ public class TestPage : ContentPage
     // Like in any other class, we can put fields and properties and stuff outside any method. They will be executed when object is initialized.
     int count = 0;
 
-    // Moved labelText declaration outside to be able to get to it in OnClickEvent method.
+    // Moved fields declaration outside to be able to get to it in OnClickEvent method.
     Label labelText;
+    Label labelText2;
+    public Button buttonCounter;
 
-	public TestPage()
+    public TestPage()
 	{
         // Turns out we want to initialize entire page in constructor (as Trevoir suggests in his course).
 
@@ -44,9 +46,9 @@ public class TestPage : ContentPage
         stacklayout.Children.Add(labelText);
 
         // Create button
-        Button buttonCounter = new Button
+        buttonCounter = new Button
         {
-            Text = "Press to count",
+            Text = "Press to count {0}",
             HorizontalOptions = LayoutOptions.Center
         };
 
@@ -57,6 +59,15 @@ public class TestPage : ContentPage
         // We add to our own EventHandler, but I guess it makes sense, probably there's inherited OnClick method or something.
         buttonCounter.Clicked += OnClickEvent;
 
+        // One more label for verification what is being send as an "sender" (this button that was pressed).
+        labelText2 = new Label
+        {
+            Text = "Sender's text: not pressed yet...",
+            FontSize = 16,
+            HorizontalOptions = LayoutOptions.Center
+        };
+        stacklayout.Children.Add(labelText2);
+
         // Now place the View (containing thing with all the elements) as "what is in this page".
         // At his point, contents of the page, as they are now set, will be visible on screen.
         this.Content = stacklayout;
@@ -65,9 +76,24 @@ public class TestPage : ContentPage
     private void OnClickEvent(object? sender, EventArgs e)
     {
         count++;
-        labelText.Text = $"Jeff clicked {count} times!";
+        this.buttonCounter.Text = $"Press to count {count}";
+        SemanticScreenReader.Announce(this.buttonCounter.Text);
 
-        // This is for screen readers, let's them know that this text has changed.
-        SemanticScreenReader.Announce(labelText.Text);
+        // labelText.Text = $"Jeff clicked {count} times!";
+        if (sender != null)
+        {
+            labelText.Text = $"Type of sender: {sender.GetType().FullName}";
+            // This is for screen readers, let's them know that this text has changed.
+            SemanticScreenReader.Announce(labelText.Text);
+
+            Button? thisButton = sender as Button;
+            if (thisButton != null)
+            {
+                labelText2.Text = $"Sender's text: '{thisButton.Text}'";
+                SemanticScreenReader.Announce(labelText2.Text);
+
+            }
+        }
+
     }
 }
