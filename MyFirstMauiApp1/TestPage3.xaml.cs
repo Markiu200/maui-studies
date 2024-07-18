8735453cf533c22816e9adfb22ee45df50d1e052
Markiu200 <markiu2007@interia.pl>
2024-07-18 20:09:12 +0200

    var: play with items widths and heights
    Value of WidthRequest doesn't seem to match device width.

--- MyFirstMauiApp1/TestPage3.xaml.cs
+++ MyFirstMauiApp1/TestPage3.xaml.cs
@@ -5,5 +5,12 @@ public partial class TestPage3 : ContentPage
 	public TestPage3()
 	{
 		InitializeComponent();
+		// Referring to labellabel label is allowed in methods, but not in constructor nor class body.
+		TurnsOutItMustBeInAnotherMethod();
+	}
+	
+	private void TurnsOutItMustBeInAnotherMethod()
+	{
+		labelLabel.Text = $"Pixel width: {DeviceDisplay.Current.MainDisplayInfo.Width}";
 	}
 }
