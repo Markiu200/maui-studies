a0acadb63ee15ab2860db2fcad137490a5aa6196
Markiu200 <markiu2007@interia.pl>
2024-07-20 15:51:07 +0200

    feat: shared resources to use in XAML, decoupled from controls

---
+++ MyFirstMauiApp1/Resources/SharedResources.cs
@@ -0,0 +1,10 @@
+﻿namespace MyFirstMauiApp1.Resources
+{
+    // Concept behind this (in course) is to have XAML Global Markup Variable, but to have it decoupled from any of the controls / pages.
+    // Now instead of defining that in MainPage, we have individual C# class as resource.
+    // Class doesn't need to be called "SharedResources", neither does it have to be in "Resources" folder, but that would make sense to put it there.
+    internal static class SharedResources
+    {
+        public static readonly Color FntColor = Colors.Firebrick;
+    }
+}
