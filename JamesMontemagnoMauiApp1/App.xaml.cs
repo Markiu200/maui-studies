6fc15cfb8ac90c93899a5bb185369992d09c045f
Markiu200 <markiu2007@interia.pl>
2024-08-07 15:02:46 +0200

    initial commit for second project
    Udemy course from Trevoir Williams gives you a lot of code that just appears and is never explained. I feel like maybe I should try with a different course for now, and then come back to Udemy and see if I'll get it then.

---
+++ JamesMontemagnoMauiApp1/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace JamesMontemagnoMauiApp1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ JamesMontemagnoMauiApp1/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace JamesMontemagnoMauiApp1
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
